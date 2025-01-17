b582065b37c15232490ca322948ce76af7968252
Tilen Komel <me@komelt.dev>
2025-01-17 20:22:05 +0100

    Update

--- tilemaker/script-server.sh
+++ tilemaker/script-server.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 
-# Clone repo
-git clone https://github.com/systemed/tilemaker.git
-
 # Run tilemaker
-/usr/src/app/tilemaker-server --input ./out/data.mbtiles --static /var/app/tilemaker/server/static
+/usr/src/app/tilemaker-server --input ./out/data.mbtiles --static /var/app/static
