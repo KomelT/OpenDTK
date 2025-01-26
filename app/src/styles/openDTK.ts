23c4436b9899fc06023664ac2214ed4873654c23
Tilen Komel <me@komelt.dev>
2025-01-26 16:21:04 +0100

    Updated waterways

--- app/src/styles/openDTK.ts
+++ app/src/styles/openDTK.ts
@@ -6,7 +6,7 @@ export const openDTK: StyleSpecification = {
 	version: 8,
 	name: "OpenDTK",
 	sources: {
-		openmaptiles: {
+		opendtk: {
 			type: "vector",
 			url: `${import.meta.env.VITE_TILEMAKER_SERVER_URL}/spec.json`,
 		},
@@ -22,235 +22,267 @@ export const openDTK: StyleSpecification = {
 	],
 };
 
-const terrains: LayerSpecification[] = [
-	{
-		id: "gozd",
-		type: "fill",
-		source: "openmaptiles",
-		"source-layer": "landcover",
-		filter: ["==", "class", "wood"],
-		paint: {
-			"fill-color": "#c8e2d0",
+// Terrain types
+openDTK.layers.push(
+	...([
+		{
+			id: "gozd",
+			type: "fill",
+			source: "opendtk",
+			"source-layer": "landcover",
+			filter: ["==", "class", "wood"],
+			paint: {
+				"fill-color": "#c8e2d0",
+			},
 		},
-	},
-	{
-		id: "zgradba",
-		type: "fill",
-		source: "openmaptiles",
-		"source-layer": "building",
-		paint: {
-			"fill-color": "#000",
+		{
+			id: "zgradba",
+			type: "fill",
+			source: "opendtk",
+			"source-layer": "building",
+			paint: {
+				"fill-color": "#000",
+			},
 		},
-	},
-	{
-		id: "voda",
-		type: "fill",
-		source: "openmaptiles",
-		"source-layer": "water",
-		paint: {
-			"fill-color": "#a9b5d6",
-		},
-	}
-];
-openDTK.layers.push(...terrains);
+	] as LayerSpecification[]),
+);
 
-const roads: LayerSpecification[] = [
-	{
-		id: "pespot",
-		type: "line",
-		source: "openmaptiles",
-		"source-layer": "transportation",
-		filter: ["all", ["==", "$type", "LineString"], ["in", "class", "path"]],
-		layout: { "line-cap": "square", "line-join": "bevel" },
-		paint: {
-			"line-color": "#000",
-			"line-dasharray": [2, 3],
+// Waterways
+openDTK.layers.push(
+	...([
+		{
+			id: "potok-kanal-ozji-5m-obcasno-brez-vode",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "waterway",
+			filter: [
+				"all",
+				["in", "class", "canal", "stream"],
+				["!=", "brunnel", "tunnel"],
+				["==", "intermittent", 1],
+			],
+			layout: { "line-cap": "round", visibility: "visible" },
+			paint: {
+				"line-color": "#a9b5d6",
+				"line-width": 1,
+				"line-dasharray": [7, 2],
+			},
 		},
-	},
-	{
-		id: "sirsa-steza",
-		type: "line",
-		source: "openmaptiles",
-		"source-layer": "transportation",
-		filter: [
-			"all",
-			["==", "$type", "LineString"],
-			["in", "tracktype", "grade5", "grade6"],
-		],
-		layout: { "line-cap": "square", "line-join": "bevel" },
-		paint: {
-			"line-color": "#000",
-			"line-dasharray": [10, 3],
+		{
+			id: "potok-kanal-ozji-5m",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "waterway",
+			filter: [
+				"all",
+				["in", "class", "canal", "stream"],
+				["!=", "brunnel", "tunnel"],
+				["==", "intermittent", 0],
+			],
+			layout: { "line-cap": "round", visibility: "visible" },
+			paint: {
+				"line-color": "#a9b5d6",
+				"line-width": 1,
+			},
 		},
-	},
-	{
-		id: "slabsi_kolovoz",
-		type: "line",
-		source: "openmaptiles",
-		"source-layer": "transportation",
-		filter: [
-			"all",
-			["==", "$type", "LineString"],
-			["in", "tracktype", "grade3", "grade4"],
-		],
-		layout: { "line-cap": "square", "line-join": "bevel" },
-		paint: {
-			"line-color": "#000",
-			"line-dasharray": [25, 3],
+		{
+			id: "vodovje",
+			type: "fill",
+			source: "opendtk",
+			"source-layer": "water",
+			filter: ["==", "$type", "Polygon"],
+			paint: {
+				"fill-color": "#a9b5d6",
+			},
 		},
-	},
-	{
-		id: "kolovoz",
-		type: "line",
-		source: "openmaptiles",
-		"source-layer": "transportation",
-		filter: [
-			"all",
-			["==", "$type", "LineString"],
-			["in", "tracktype", "grade1", "grade2"],
-		],
-		layout: { "line-cap": "square", "line-join": "bevel" },
-		paint: {
-			"line-color": "#000",
-		},
-	},
-	{
-		id: "lokalna_cesta",
-		type: "line",
-		source: "openmaptiles",
-		"source-layer": "transportation",
-		filter: ["all", ["==", "$type", "LineString"], ["in", "class", "minor"]],
-		layout: { "line-cap": "square", "line-join": "bevel" },
-		paint: {
-			"line-color": "#000",
-			"line-gap-width": 1,
-		},
-	},
-	{
-		id: "lokalna_cesta_bg",
-		type: "line",
-		source: "openmaptiles",
-		"source-layer": "transportation",
-		filter: ["all", ["==", "$type", "LineString"], ["in", "class", "minor"]],
-		layout: { "line-cap": "square", "line-join": "bevel" },
-		paint: {
-			"line-color": "#fff",
-			"line-width": 1,
-		},
-	},
-	{
-		id: "obcinska_cesta",
-		type: "line",
-		source: "openmaptiles",
-		"source-layer": "transportation",
-		filter: ["all", ["==", "$type", "LineString"], ["in", "class", "tertiary"]],
-		layout: { "line-cap": "square", "line-join": "bevel" },
-		paint: {
-			"line-color": "#000",
-			"line-gap-width": 2,
-		},
-	},
-	{
-		id: "drzavna_cesta",
-		type: "line",
-		source: "openmaptiles",
-		"source-layer": "transportation",
-		filter: [
-			"all",
-			["==", "$type", "LineString"],
-			["in", "class", "secondary"],
-		],
-		layout: { "line-cap": "square", "line-join": "bevel" },
-		paint: {
-			"line-color": "#000",
-			"line-gap-width": 2,
-		},
-	},
-	{
-		id: "regionalna_cesta",
-		type: "line",
-		source: "openmaptiles",
-		"source-layer": "transportation",
-		filter: ["all", ["==", "$type", "LineString"], ["in", "class", "primary"]],
-		layout: { "line-cap": "square", "line-join": "bevel" },
-		paint: {
-			"line-color": "#000",
-			"line-gap-width": 2,
-		},
-	},
-	{
-		id: "hitra_cesta",
-		type: "line",
-		source: "openmaptiles",
-		"source-layer": "transportation",
-		filter: ["all", ["==", "$type", "LineString"], ["in", "class", "trunk"]],
-		layout: { "line-cap": "square", "line-join": "bevel" },
-		paint: {
-			"line-color": "#000",
-			"line-gap-width": 2,
-		},
-	},
-	{
-		id: "avto_cesta",
-		type: "line",
-		source: "openmaptiles",
-		"source-layer": "transportation",
-		filter: ["all", ["==", "$type", "LineString"], ["in", "class", "motorway"]],
-		layout: { "line-cap": "square", "line-join": "bevel" },
-		paint: {
-			"line-color": "#000",
-			"line-gap-width": 2,
-		},
-	},
-	{
-		id: "cesta_bg",
-		type: "line",
-		source: "openmaptiles",
-		"source-layer": "transportation",
-		filter: [
-			"all",
-			["==", "$type", "LineString"],
-			["in", "class", "tertiary", "secondary", "primary", "trunk", "motorway"],
-		],
-		layout: { "line-cap": "square", "line-join": "bevel" },
-		paint: {
-			"line-color": "#fff",
-			"line-width": 2,
-		},
-	},
-];
-openDTK.layers.push(...roads);
+	] as LayerSpecification[]),
+);
 
-const labels: LayerSpecification[] = [
-	{
-		id: "place-other",
-		type: "symbol",
-		metadata: { "mapbox:group": "1444849242106.713" },
-		source: "openmaptiles",
-		"source-layer": "place",
-		filter: [
-			"!in",
-			"class",
-			"city",
-			"town",
-			"village",
-			"state",
-			"country",
-			"continent",
-		],
-		layout: {
-			"text-font": ["KlokanTech Noto Sans Bold"],
-			"text-letter-spacing": 0.1,
-			"text-max-width": 9,
-			"text-size": 12,
-			"text-transform": "uppercase",
-			visibility: "visible",
+// Roads
+openDTK.layers.push(
+	...([
+		{
+			id: "pespot",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "transportation",
+			filter: ["all", ["==", "$type", "LineString"], ["in", "class", "path"]],
+			layout: { "line-cap": "square", "line-join": "bevel" },
+			paint: {
+				"line-color": "#000",
+				"line-dasharray": [2, 3],
+			},
 		},
-		paint: {
-			"text-color": "#633",
-			"text-halo-color": "rgba(255,255,255,0.8)",
-			"text-halo-width": 1.2,
+		{
+			id: "sirsa-steza",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "transportation",
+			filter: [
+				"all",
+				["==", "$type", "LineString"],
+				["in", "tracktype", "grade5", "grade6"],
+			],
+			layout: { "line-cap": "square", "line-join": "bevel" },
+			paint: {
+				"line-color": "#000",
+				"line-dasharray": [10, 3],
+			},
 		},
-	},
-];
-openDTK.layers.push(...labels);
+		{
+			id: "slabsi_kolovoz",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "transportation",
+			filter: [
+				"all",
+				["==", "$type", "LineString"],
+				["in", "tracktype", "grade3", "grade4"],
+			],
+			layout: { "line-cap": "square", "line-join": "bevel" },
+			paint: {
+				"line-color": "#000",
+				"line-dasharray": [25, 3],
+			},
+		},
+		{
+			id: "kolovoz",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "transportation",
+			filter: [
+				"all",
+				["==", "$type", "LineString"],
+				["in", "tracktype", "grade1", "grade2"],
+			],
+			layout: { "line-cap": "square", "line-join": "bevel" },
+			paint: {
+				"line-color": "#000",
+			},
+		},
+		{
+			id: "lokalna_cesta",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "transportation",
+			filter: ["all", ["==", "$type", "LineString"], ["in", "class", "minor"]],
+			layout: { "line-cap": "square", "line-join": "bevel" },
+			paint: {
+				"line-color": "#000",
+				"line-gap-width": 1,
+			},
+		},
+		{
+			id: "lokalna_cesta_bg",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "transportation",
+			filter: ["all", ["==", "$type", "LineString"], ["in", "class", "minor"]],
+			layout: { "line-cap": "square", "line-join": "bevel" },
+			paint: {
+				"line-color": "#fff",
+				"line-width": 1,
+			},
+		},
+		{
+			id: "obcinska_cesta",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "transportation",
+			filter: [
+				"all",
+				["==", "$type", "LineString"],
+				["in", "class", "tertiary"],
+			],
+			layout: { "line-cap": "square", "line-join": "bevel" },
+			paint: {
+				"line-color": "#000",
+				"line-gap-width": 2,
+			},
+		},
+		{
+			id: "drzavna_cesta",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "transportation",
+			filter: [
+				"all",
+				["==", "$type", "LineString"],
+				["in", "class", "secondary"],
+			],
+			layout: { "line-cap": "square", "line-join": "bevel" },
+			paint: {
+				"line-color": "#000",
+				"line-gap-width": 2,
+			},
+		},
+		{
+			id: "regionalna_cesta",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "transportation",
+			filter: [
+				"all",
+				["==", "$type", "LineString"],
+				["in", "class", "primary"],
+			],
+			layout: { "line-cap": "square", "line-join": "bevel" },
+			paint: {
+				"line-color": "#000",
+				"line-gap-width": 2,
+			},
+		},
+		{
+			id: "hitra_cesta",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "transportation",
+			filter: ["all", ["==", "$type", "LineString"], ["in", "class", "trunk"]],
+			layout: { "line-cap": "square", "line-join": "bevel" },
+			paint: {
+				"line-color": "#000",
+				"line-gap-width": 2,
+			},
+		},
+		{
+			id: "avto_cesta",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "transportation",
+			filter: [
+				"all",
+				["==", "$type", "LineString"],
+				["in", "class", "motorway"],
+			],
+			layout: { "line-cap": "square", "line-join": "bevel" },
+			paint: {
+				"line-color": "#000",
+				"line-gap-width": 2,
+			},
+		},
+		{
+			id: "cesta_bg",
+			type: "line",
+			source: "opendtk",
+			"source-layer": "transportation",
+			filter: [
+				"all",
+				["==", "$type", "LineString"],
+				[
+					"in",
+					"class",
+					"tertiary",
+					"secondary",
+					"primary",
+					"trunk",
+					"motorway",
+				],
+			],
+			layout: { "line-cap": "square", "line-join": "bevel" },
+			paint: {
+				"line-color": "#fff",
+				"line-width": 2,
+			},
+		},
+	] as LayerSpecification[]),
+);
