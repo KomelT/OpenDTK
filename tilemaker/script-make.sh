b13d6a152b6c4b10a6eca0b7ec067a5fc115ceaf
Tilen Komel <me@komelt.dev>
2025-01-27 13:09:32 +0100

    Add contours download

--- tilemaker/script-make.sh
+++ tilemaker/script-make.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 OSM_PBF_FILE="https://mirror.komelt.dev/osm/europe/slovenia-latest.osm.pbf"
+CONTOUR_SHP_FILE="https://mirror.komelt.dev/gurs/DTM_SLO_RELIEF.zip"
 
 # Chech if ./out/data.mbtiles exists
 if [ -f "./out/data.mbtiles" ]; then
@@ -8,7 +9,7 @@ if [ -f "./out/data.mbtiles" ]; then
   exit 0
 fi
 
-mkdir -p ./tmp
+mkdir ./tmp
 
 # Check if OSM PBF file exists
 if [ -f "./tmp/data.osm.pbf" ]; then
@@ -16,6 +17,37 @@ if [ -f "./tmp/data.osm.pbf" ]; then
 else
   wget -O ./tmp/data.osm.pbf $OSM_PBF_FILE
 fi
+
+if [ $? -ne 0 ]; then
+  echo "Downloading OSM PBF file failed"
+  exit 1
+fi
+
+# Check if contour SHP files exists
+if [ -f "./tmp/DTM_SLO_RELIEF_EL_PLASTNICE_VZHOD_L_line.shp" ] && [ -f "./tmp/DTM_SLO_RELIEF_EL_PLASTNICE_ZAHOD_L_line.shp" ]; then
+  echo "File ./tmp/data.osm.pbf already exists. Skipping download..."
+else
+  if [ -f "./tmp/DTM_SLO_RELIEF.zip" ]; then
+    echo "File ./tmp/DTM_SLO_RELIEF.zip already exists. Skipping download..."
+  else
+    wget $CONTOUR_SHP_FILE -O ./tmp/DTM_SLO_RELIEF.zip
+    if [ $? -ne 0 ]; then
+      echo "Failed to download contour SHP files"
+      exit 1
+    fi
+  fi
+
+  unzip ./tmp/DTM_SLO_RELIEF.zip -d ./DTM_SLO_RELIEF
+  unzip ./DTM_SLO_RELIEF/DTM_SLO_RELIEF_EL_PLASTNICE_* -d ./tmp_contour
+
+  mv ./tmp_contour/*.shp ./tmp/
+  mv ./tmp_contour/*.shx ./tmp/
+
+  rm -rf ./DTM_SLO_RELIEF*
+  rm -rf ./tmp_contour
+fi
+
+echo -e "Done\n"
 
 # Run tilemaker
 /usr/src/app/tilemaker ./tmp/data.osm.pbf \
